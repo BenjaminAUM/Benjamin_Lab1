165c9ed94b6971207bb60ebd73085d5a2e83ed52
colby209 <cwill209@aum.edu>
2026-04-08 11:12:06 -0500

    Student B changes greeting

--- Benjamin_Lab1/Benjamin_Lab1.cpp
+++ Benjamin_Lab1/Benjamin_Lab1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Colby! This is my first Git commit." << endl;
+    cout << "Hello from Student B!." << endl;
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Colby!" << endl;
     cout << "Lab 1 completed on 4-6-26!" << endl;
